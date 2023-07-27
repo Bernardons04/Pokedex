48d0bdb7b45e1c0fbf07976d1115e9def86c4ffe
Bernardo Araujo Nascimento <112490065+Bernardons04@users.noreply.github.com>
2023-07-27 14:56:34 -0300

    Update pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -52,7 +52,7 @@ const mostrarPokemon = async (pokemon) => {
     }
 }
 
-input.addEventListener("keyup", (e) => {
+input.addEventListener("keydown", (e) => {
     if(e.keyCode === 13) {
         e.preventDefault();
         mostrarPokemon(input.value);
